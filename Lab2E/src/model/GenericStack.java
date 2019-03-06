7936a72090788088225bb0cbfc3c30e84695beda
Jose <joseossorio99@hotmail.com>
2019-03-06 17:44:57 -0500

    finished generic Stack class

--- Lab2E/src/model/GenericStack.java
+++ Lab2E/src/model/GenericStack.java
@@ -5,11 +5,56 @@ import java.util.ArrayList;
 public class GenericStack<T> {
 
 	private ArrayList<T> stack;
-	
+
 	public GenericStack() {
-		
+
 		stack = new ArrayList<T>();
-		
+
+	}
+
+	public GenericStack(T[] arr) {
+
+		stack = new ArrayList<T>();
+		for (int i = 0; i < arr.length; i++) {
+			stack.add(arr[i]);
+		}
+
+	}
+
+	public ArrayList<T> getStack() {
+		return stack;
+	}
+
+	public void setStack(ArrayList<T> stack) {
+		this.stack = stack;
+	}
+
+	public T peek() {
+		return stack.get(stack.size() - 1);
+	}
+
+	public T pop() {
+		T r = stack.get(stack.size() - 1);
+		stack.remove(r);
+		return r;
+	}
+
+	public void push(T elem) {
+		stack.add(elem);
+	}
+
+	public int search(T elem) {
+		int r = -1;
+		for (int i = 0; i < stack.size(); i++) {
+			if (stack.get(i).equals(elem)) {
+				r = i;
+			}
+		}
+		return r;
+	}
+
+	public boolean isEmpty() {
+		return (stack.isEmpty() ? true : false);
 	}
 
 }
