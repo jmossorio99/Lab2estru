0a2540b2922ac262b288dbed407d1456214663c2
Jose Manuel Ossorio Trochez <1144110009@academico.icesi.edu.co>
2019-03-08 12:47:30 -0500

    added Store class

--- Lab2E/src/model/Store.java
+++ Lab2E/src/model/Store.java
@@ -1,5 +1,15 @@
 package model;
 
+import threads.CashierThread;
+
 public class Store {
+
+	private Client[] clients;
+	private CashierThread[] cashierThreads;
+	// private GenericMinPriorityQueue queue;
+
+	public Store(int clientNum, int cashierNum, int shelfNum) {
+
+	}
 
 }
