45a4e656eefc281a6a6634f222291653df4dc315
Jose Manuel Ossorio Trochez <1144110009@academico.icesi.edu.co>
2019-03-08 12:19:12 -0500

    added threads package and store class

--- Lab2E/src/model/GenericMinHeap.java
+++ Lab2E/src/model/GenericMinHeap.java
@@ -5,86 +5,87 @@ public class GenericMinHeap<T extends Comparable> {
 	private T[] heap;
 	private int size;
 	private int maxsize;
-	
+
+	@SuppressWarnings("unchecked")
 	public GenericMinHeap(T[] array, int maxsize) {
 		this.size = array.length;
 		this.maxsize = maxsize;
 		heap = (T[]) new Comparable[maxsize];
-		for(int i = 0; i < array.length; i++) {
-			heap[i+1] = array[i];
+		for (int i = 0; i < array.length; i++) {
+			heap[i + 1] = array[i];
 		}
 		buildMinHeap();
 	}
-	
+
 	private int parent(int position) {
 		return position / 2;
 	}
-	
+
 	private int leftChild(int position) {
 		return 2 * position;
 	}
-	
+
 	private int rightChild(int position) {
 		return (2 * position) + 1;
 	}
-	
+
 	public int getSize() {
 		return size;
 	}
-	
+
 	public T get(int pos) {
 		return heap[pos];
 	}
-	
+
 	private void exchange(int pos1, int pos2) {
 		T temp = heap[pos1];
 		heap[pos1] = heap[pos2];
 		heap[pos2] = temp;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	private void minHeapify(int pos) {
 		int smallest;
 		int left = leftChild(pos);
 		int right = rightChild(pos);
-		if(left <= size && heap[left].compareTo(heap[pos]) < 0) {
+		if (left <= size && heap[left].compareTo(heap[pos]) < 0) {
 			smallest = left;
-		}else {
+		} else {
 			smallest = pos;
 		}
-		if(right <= size && heap[right].compareTo(heap[smallest]) < 0) {
+		if (right <= size && heap[right].compareTo(heap[smallest]) < 0) {
 			smallest = right;
 		}
-		if(smallest != pos) {
+		if (smallest != pos) {
 			exchange(smallest, pos);
 			minHeapify(smallest);
 		}
 	}
-	
+
 	private void buildMinHeap() {
-		for(int i = size/2; i >= 1; i--) {
+		for (int i = size / 2; i >= 1; i--) {
 			minHeapify(i);
 		}
 	}
-	
+
 	public void insert(T elem) {
 		heap[++size] = elem;
 		int current = size;
-		while(size > 1 && heap[current].compareTo(heap[parent(current)]) < 0) {
+		while (size > 1 && heap[current].compareTo(heap[parent(current)]) < 0) {
 			exchange(current, parent(current));
 			current = parent(current);
 		}
 	}
-	
+
 	public void set(int pos, T elem) {
 		heap[pos] = elem;
 	}
-	
+
 	public T remove(int pos) {
 		T removed = heap[pos];
 		heap[pos] = heap[size--];
 		minHeapify(pos);
 		return removed;
 	}
-	
+
 }
