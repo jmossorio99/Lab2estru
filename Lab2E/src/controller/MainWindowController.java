52891d3f31d008fed210a68d729aefb9b77e8155
Jose <joseossorio99@hotmail.com>
2019-03-12 12:09:39 -0500

    changed the window that opens when you click the button the main window

--- Lab2E/src/controller/MainWindowController.java
+++ Lab2E/src/controller/MainWindowController.java
@@ -5,6 +5,7 @@ import javax.swing.JOptionPane;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -26,31 +27,27 @@ public class MainWindowController {
 	private Button nextBtn;
 	private int cashierNum = 0;
 	private int shelfNum = 0;
-	private boolean popped = false;
 
 	@FXML
 	void nextBtnClicked(ActionEvent event) {
 
-		//if (!cashierNumTxt.getText().isEmpty()) {
+		// if (!cashierNumTxt.getText().isEmpty()) {
 
-			//if (!shelfNumTxt.getText().isEmpty()) {
-
-				if (!popped) {
+		// if (!shelfNumTxt.getText().isEmpty()) {
 
 //					cashierNum = Integer.parseInt(cashierNumTxt.getText());
 //					shelfNum = Integer.parseInt(shelfNumTxt.getText());
-					FXMLLoader loader = new FXMLLoader(getClass().getResource(("/view/PopUpWindowView.fxml")));
-					try {
-						Parent root = loader.load();
-						Stage popUp = new Stage();
-						Scene popUpScene = new Scene(root);
-						popUp.setScene(popUpScene);
-						popUp.show();
-						popped = true;
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
+		FXMLLoader loader = new FXMLLoader(getClass().getResource(("/view/BasicInformationWindow.fxml")));
+		try {
+			Parent root = loader.load();
+			Stage window = (Stage) (((Node) event.getSource()).getScene().getWindow());
+			Scene popUpScene = new Scene(root);
+			window.setScene(popUpScene);
+			window.show();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 //			} else {
 //				JOptionPane.showMessageDialog(null, "Please insert the number of shelves", "Error",
