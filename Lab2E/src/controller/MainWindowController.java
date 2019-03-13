52983a4148735caf859098f045026bfa88256c34
Jose <joseossorio99@hotmail.com>
2019-03-13 18:45:12 -0500

    few changes to the mainWindowController

--- Lab2E/src/controller/MainWindowController.java
+++ Lab2E/src/controller/MainWindowController.java
@@ -1,7 +1,12 @@
 package controller;
 
+import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import javax.swing.JOptionPane;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
@@ -21,17 +26,48 @@ public class MainWindowController {
 	@FXML
 	private Button goBtn;
 	private FileChooser fc = new FileChooser();
+	private int numOfCases = 0;
 
 	@FXML
 	void exploreBtnClicked(ActionEvent event) {
 
 		File file = fc.showOpenDialog((Stage) (((Node) event.getSource()).getScene().getWindow()));
+		readFile(file);
 		System.out.println(file.getAbsolutePath().toString());
 
 	}
 
 	@FXML
 	void goBtnClicked(ActionEvent event) {
+
+	}
+
+	public void readFile(File file) {
+
+		String fileName = file.getName();
+		if (fileName.contains(".txt")) {
+
+			try {
+
+				FileInputStream fis = new FileInputStream(file);
+				BufferedReader buff = new BufferedReader(new InputStreamReader(fis));
+				numOfCases = Integer.parseInt(buff.readLine());
+				for (int i = 0; i < numOfCases; i++) {
+					
+				}
+
+			} catch (FileNotFoundException e) {
+				JOptionPane.showMessageDialog(null, "Error readign the file", "Error", JOptionPane.ERROR_MESSAGE);
+			} catch (NumberFormatException e) {
+				JOptionPane.showMessageDialog(null, "The first line of the file must be a number", "Error",
+						JOptionPane.ERROR_MESSAGE);
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(null, "Error readign the file", "Error", JOptionPane.ERROR_MESSAGE);
+			}
+
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong file format", "Error", JOptionPane.ERROR_MESSAGE);
+		}
 
 	}
 
