5bea75eca1c7eab3fec96645f0ee06936681395c
Jose <joseossorio99@hotmail.com>
2019-03-13 14:13:35 -0500

    Changed the interface, now it will only consist of an explore button to locate the text file with the data for the program

--- Lab2E/src/controller/MainWindowController.java
+++ Lab2E/src/controller/MainWindowController.java
@@ -1,63 +1,37 @@
 package controller;
 
-import java.io.IOException;
-import javax.swing.JOptionPane;
+import java.io.File;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class MainWindowController {
 
 	@FXML
-	private Label cashierNumLbl;
-	@FXML
-	private TextField cashierNumTxt;
-	@FXML
-	private Label shelfNumLbl;
-	@FXML
-	private TextField shelfNumTxt;
-	@FXML
-	private Button nextBtn;
-	private int cashierNum = 0;
-	private int shelfNum = 0;
+	private Button exploreBtn;
 
 	@FXML
-	void nextBtnClicked(ActionEvent event) {
+	private TextField routeTxt;
 
-		// if (!cashierNumTxt.getText().isEmpty()) {
+	@FXML
+	private Button goBtn;
+	private FileChooser fc = new FileChooser();
 
-		// if (!shelfNumTxt.getText().isEmpty()) {
+	@FXML
+	void exploreBtnClicked(ActionEvent event) {
 
-//					cashierNum = Integer.parseInt(cashierNumTxt.getText());
-//					shelfNum = Integer.parseInt(shelfNumTxt.getText());
-		FXMLLoader loader = new FXMLLoader(getClass().getResource(("/view/BasicInformationWindow.fxml")));
-		try {
-			Parent root = loader.load();
-			Stage window = (Stage) (((Node) event.getSource()).getScene().getWindow());
-			Scene popUpScene = new Scene(root);
-			window.setScene(popUpScene);
-			window.show();
+		File file = fc.showOpenDialog((Stage) (((Node) event.getSource()).getScene().getWindow()));
+		System.out.println(file.getAbsolutePath().toString());
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	}
 
-//			} else {
-//				JOptionPane.showMessageDialog(null, "Please insert the number of shelves", "Error",
-//						JOptionPane.ERROR_MESSAGE);
-//			}
-
-//		} else {
-//			JOptionPane.showMessageDialog(null, "Please insert the number of cashiers", "Error",
-//					JOptionPane.ERROR_MESSAGE);
-//		}
+	@FXML
+	void goBtnClicked(ActionEvent event) {
 
 	}
 
